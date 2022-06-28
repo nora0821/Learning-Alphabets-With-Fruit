9b8c85e1b0b61b13e697a8154d76bcdb5a6b7e2d
nora0821 <104416584+nora0821@users.noreply.github.com>
2022-06-28 12:03:25 +0800

    Update Guess That Fruit.py

--- Guess That Fruit.py
+++ Guess That Fruit.py
@@ -145,25 +145,25 @@ def main():
         else:
             print("You guessed {}. Come On! !\U0001F612".format(guess))
 
-fruit = [
-        "Contain pottasium",]
+    fruit = [
+            "Contain pottasium",]
 
-    x = random.choice(fruit)
-    print("Choose the correct answer below")
-    print("\nANSWERS OPTIONS: Contain Pottasium, Contain Zinc, Good For Digestion")
-    print("\nPlease write the answer exactly like the answers options")
-    
-    guess = None
+        x = random.choice(fruit)
+        print("Choose the correct answer below")
+        print("\nANSWERS OPTIONS: Contain Pottasium, Contain Zinc, Good For Digestion")
+        print("\nPlease write the answer exactly like the answers options")
 
-    while x != guess:
+        guess = None
 
-            guess = str(input("HARD: State the benefit for eating banana? \0001F619"))
-            
-            if x == guess:
-                print("\nYou guessed {}. Hurray! Correct! \U0001F601".format(guess))
-                break
-            else:
-                print("\nYou guessed {}. Come On! !\U0001F612".format(guess))
+        while x != guess:
+
+                guess = str(input("HARD: State the benefit for eating banana? \0001F619"))
+
+                if x == guess:
+                    print("\nYou guessed {}. Hurray! Correct! \U0001F601".format(guess))
+                    break
+                else:
+                    print("\nYou guessed {}. Come On! !\U0001F612".format(guess))
 
     
 
